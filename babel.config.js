dc28a8fc09ebb4ea014cdf45c500d0a11e391a1a
E-Martynenko1982 <motor8103@gmail.com>
2025-03-16 20:18:36 +0200

    update

--- babel.config.js
+++ babel.config.js
@@ -5,5 +5,16 @@ module.exports = function (api) {
       ["babel-preset-expo", { jsxImportSource: "nativewind" }],
       "nativewind/babel",
     ],
+    plugins: [
+      [
+        "module-resolver",
+        {
+          root: ["."],
+          alias: {
+            "@": "./", // Adjust if your intended alias target is different
+          },
+        },
+      ],
+    ],
   };
 };
